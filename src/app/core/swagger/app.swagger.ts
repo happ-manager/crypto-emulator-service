f3ace532c8fd81eed948699f17a257e737c56b64
Viacheslav Saloid <viacheslavsaloid.work@gmail.com>
2025-01-22 21:13:52 +0200

    fix: main

--- src/app/core/swagger/app.swagger.ts
+++ src/app/core/swagger/app.swagger.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { PREFIX, SWAGGER } from "../../shared/constants/prefix.constant";
 
 export function appSwagger(app: INestApplication) {
-	const config = new DocumentBuilder().setTitle("Happ Crypto").addBearerAuth().build();
+	const config = new DocumentBuilder().setTitle("Emulator Service").addBearerAuth().build();
 	const document = SwaggerModule.createDocument(app, config);
 	SwaggerModule.setup(`${PREFIX}/${SWAGGER}`, app, document);
 }
